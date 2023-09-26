17763a62c204d1ff0dfbc8cdf23bbda67c4c2758
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-09-26 22:47:56 +0600

    created usersSlice

---
+++ redux/users/usersSlice.js
@@ -0,0 +1,46 @@
+const { createSlice, createAsyncThunk } = require('@reduxjs/toolkit');
+
+// initial state
+const initialState = {
+	loading: false,
+	users: [],
+	error: '',
+};
+
+// users thunk
+const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+	const response = await fetch(
+		'https://jsonplaceholder.typicode.com/albums?_limit=5'
+	);
+	const users = await response.json();
+
+	return users;
+});
+
+// create user slice
+const usersSlice = createSlice({
+	name: 'users',
+	initialState,
+	extraReducers: (builder) => {
+		builder.addCase(fetchUsers.pending, (state, action) => {
+			state.loading = true;
+			state.error = '';
+		});
+
+		builder.addCase(fetchUsers.fulfilled, (state, action) => {
+			state.loading = false;
+			state.users = action.payload;
+			state.error = '';
+		});
+
+		builder.addCase(fetchUsers.rejected, (state, action) => {
+			state.loading = false;
+			state.error = action.error.message;
+		});
+	},
+});
+
+module.exports = {
+	default: usersSlice,
+	fetchUsers: fetchUsers,
+};
